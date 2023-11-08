eb166d41e1eb09c18244a35fa56e07f572798b48
1ranxu <263989312@qq.com>
2023-11-08 09:47:36 +0800

    通用项目模板搭建：开发通用布局（底部footer布局优化，优化content的样式）

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="basiclayout">
-    <a-layout style="height: 400px">
+    <a-layout style="min-height: 100vh">
       <a-layout-header class="header">
         <GlobalHeader />
       </a-layout-header>
@@ -28,12 +28,13 @@
 #basiclayout .content {
   background: linear-gradient(to right, #bbb, #fff);
   margin-bottom: 16px;
+  padding: 20px;
 }
 
 #basiclayout .footer {
   background: #efefef;
   padding: 16px;
-  position: absolute;
+  position: sticky;
   bottom: 0;
   left: 0;
   right: 0;
