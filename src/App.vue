5e2373540ce1dc629414d3f55fba6f69ce56028a
1ranxu <263989312@qq.com>
2023-11-08 19:47:23 +0800

    通用项目模板搭建：前后端联调-后端接口调用代码自动生成

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,26 @@
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import AccessEnum from "@/access/AccessEnum";
+//全局初始化函数，全局单次调用的代码都可以携带这里
+const doInit = () => {
+  console.log("hello");
+};
+onMounted(() => {
+  doInit();
+});
+const router = useRouter();
+const store = useStore();
+router.beforeEach((to, from, next) => {
+  if (to.meta.access === AccessEnum.ADMIN) {
+    if (store.state.user.loginUser.userRole !== AccessEnum.ADMIN) {
+      next("/noAuth");
+      return;
+    }
+  }
+  next();
+});
 </script>
