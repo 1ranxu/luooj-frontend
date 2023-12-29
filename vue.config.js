1bcb8c57ae655507e7ed3ae94343c53c63c67958
1ranxu <263989312@qq.com>
2023-12-29 21:00:23 +0800

    fix:解决ResizeObserver loop

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,24 @@
 const { defineConfig } = require("@vue/cli-service");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 module.exports = defineConfig({
+  devServer: {
+    client: {
+      overlay: {
+        runtimeErrors: (error) => {
+          const ignoreErrors = [
+            "ResizeObserver loop limit exceeded",
+            "ResizeObserver loop completed with undelivered notifications.",
+          ];
+          if (ignoreErrors.includes(error.message)) {
+            return false;
+          }
+          return false;
+        },
+      },
+    },
+  },
   transpileDependencies: true,
+  lintOnSave: false,
   chainWebpack(config) {
     config.plugin("monaco").use(new MonacoWebpackPlugin());
   },
